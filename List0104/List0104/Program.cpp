347e8b0f6f56a2dd18933787455edc66c9b15deb
takumi <t93gochi93@gmail.com>
2024-10-16 15:38:59 +0900

    [Add]クライアント領域 クライアント領域の指定設定

--- List0104/List0104/Program.cpp
+++ List0104/List0104/Program.cpp
@@ -4,27 +4,31 @@
 //=============================================================================
 #include <Windows.h>
 
-//アプリケーションのエントリーポイント
+// アプリケーションのエントリーポイント
 int WINAPI wWinMain(
 	_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPWSTR lpCmdLine, _In_ int nCmdShow) 
+	_In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
 	//ウィンドウハンドル
 	HWND hWnd = NULL;
 
-	//ウィンドウの作成
-	hWnd = CreateWindowExW(
-		0, L"Static", L"サンプル", WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, 640, 480,
-		NULL, NULL, hInstance, NULL);
+	//ウィンドウの大きさ計算
+	RECT rect = { 0, 0, 640, 480 };
+	AdjustWindowRectEx(&rect, WS_OVERLAPPEDWINDOW, FALSE, 0);
 
+	//ウィンドウの作成
+	hWnd = CreateWindowEx(
+		0, L"Static", L"タイトル", WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		(rect.right - rect.left), (rect.bottom - rect.top), //ウィンドウ幅計算
+		NULL, NULL, hInstance, NULL
+	);
 	//hWndの正常性確認
 	if (hWnd == NULL) {
 		MessageBox(NULL, L"ウィンドウの作成に失敗しました。", L"エラー", MB_OK);
 		return 0;
 	}
-	
-	//ウィンドウの表示と更新
+
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
 
