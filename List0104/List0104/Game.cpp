95eaf597beb8d57babdb5b0a8195f1b9866cd6a7
takumi <t93gochi93@gmail.com>
2024-11-20 12:22:13 +0900

    [Add]List03 デバイスとデバイスコンテキスト スワップチェーン バックバッファーとレンダーターゲット 深度ステンシル シェーダーリソース ビューポート 初期化

--- List0104/List0104/Game.cpp
+++ List0104/List0104/Game.cpp
@@ -2,7 +2,7 @@
 // Game.cpp
 // 
 //=============================================================================
-#include <d3d11.h>
+//#include <d3d11.h> Game.hにこの機能を使用するコードを移動したため
 #include "Game.h"
 
 // 関数のプロトタイプ宣言
@@ -61,7 +61,7 @@ bool Game::InitWindow()
 	ShowWindow(hWnd, SW_SHOWNORMAL);
 	UpdateWindow(hWnd);
 
-	// この場合は、thisキーワードを省略できない。
+	// この場合、thisを省略できないため記載
 	this->hWnd = hWnd;
 
 	return true;
@@ -72,7 +72,7 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg) {
 	case WM_CLOSE:
-		// ウィンドウを閉じる
+		// ウィンドウを閉じたとき確認用のメッセージボックスを表示
 		if (MessageBox(hWnd, L"ウィンドウを閉じますか？", L"メッセージ", MB_OKCANCEL) == IDOK) {
 			DestroyWindow(hWnd);
 		}
@@ -87,24 +87,11 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
 
-// メッセージループを実行
-int Game::Run()
+// グラフィックデバイスを作成。
+bool Game::InitGraphicsDevice()
 {
-	// ウィンドウを作成
-	if (!InitWindow()) {
-		MessageBox(NULL, L"ウィンドウの作成に失敗しました。", L"メッセージ", MB_OK);
-		return -1;
-	}
-
-	// Direct3D 11のデバイス(パソコンのグラフィック機能そのもの、GPU)
-	ID3D11Device* graphicsDevice = nullptr;
-	// Direct3D 11のデバイス コンテキスト、
-	ID3D11DeviceContext* immediateContext = nullptr;
-	// Direct3D 11の機能レベル
-	D3D_FEATURE_LEVEL featureLevel = {};
-
-
-	HRESULT hr;	// 関数の実行結果を受け取る変数
+	// 関数の実行結果を受け取る変数
+	HRESULT hr = S_OK;
 
 	// デバイス作成時のオプションフラグ
 	UINT creationFlags = 0;
@@ -113,36 +100,197 @@ int Game::Run()
 	creationFlags |= D3D11_CREATE_DEVICE_DEBUG;
 #endif
 
-	// 作成する機能レベル。希望順に指定する
-	const D3D_FEATURE_LEVEL featureLevels[] = {
-		D3D_FEATURE_LEVEL_11_0,
-		D3D_FEATURE_LEVEL_10_1,
-		D3D_FEATURE_LEVEL_10_0,
-		D3D_FEATURE_LEVEL_9_3,
-		D3D_FEATURE_LEVEL_9_2,
-		D3D_FEATURE_LEVEL_9_1,
-	};
+	// 作成するスワップチェーンについての記述
+	DXGI_SWAP_CHAIN_DESC swapChainDesc = {};
+	swapChainDesc.BufferDesc.Width = ScreenWidth;
+	swapChainDesc.BufferDesc.Height = ScreenHeight;
+	swapChainDesc.BufferDesc.RefreshRate = { 60, 1 };
+	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.SampleDesc = { 1, 0 };
+	swapChainDesc.BufferUsage =
+		DXGI_USAGE_RENDER_TARGET_OUTPUT |
+		DXGI_USAGE_SHADER_INPUT;	// シェーダーリソースとして使用することを設定
+	swapChainDesc.BufferCount = 2;
+	swapChainDesc.OutputWindow = hWnd;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
+	swapChainDesc.Windowed = TRUE;
 
-	// デバイス、デバイスコンテキストを作成
-	hr = D3D11CreateDevice(
-		NULL,						
-		D3D_DRIVER_TYPE_HARDWARE,	
-		0,							
-		creationFlags,				
-		featureLevels,				
-		6,							
-		D3D11_SDK_VERSION,			
-		&graphicsDevice,			
-		&featureLevel,				
-		&immediateContext);			
+	// デバイス、デバイスコンテキスト、スワップチェーンを作成
+	hr = D3D11CreateDeviceAndSwapChain(
+		NULL, D3D_DRIVER_TYPE_HARDWARE, 0, creationFlags, NULL, 0, D3D11_SDK_VERSION, &swapChainDesc,
+		&swapChain, &graphicsDevice, &featureLevel, &immediateContext);
 	if (FAILED(hr)) {
-		MessageBox(hWnd, L"Direct3D 11デバイスを作成できませんでした。", L"エラー", MB_OK);
+		return 0;
+	}
+
+	// バックバッファーを取得
+	ID3D11Texture2D* backBuffer = nullptr;
+	hr = swapChain->GetBuffer(0, IID_PPV_ARGS(&backBuffer));
+	//失敗したら処理しない
+	if (FAILED(hr)) {
+		return 0;
+	}
+	// バックバッファーにアクセスするためのレンダーターゲット ビューを作成
+	hr = graphicsDevice->CreateRenderTargetView(backBuffer, NULL, &renderTargetViews[0]);
+	//失敗したら処理しない
+	if (FAILED(hr)) {
+		return 0;
+	}
+	// バックバッファーにシェーダーからアクセスするためのリソース ビューを作成
+	hr = graphicsDevice->CreateShaderResourceView(
+		backBuffer,
+		NULL,
+		&renderTargetResourceView);
+	//失敗したら処理しない
+	if (FAILED(hr)) {
+		return 0;
+	}
+	SAFE_RELEASE(backBuffer);
+
+	// テクスチャとシェーダーリソースビューのフォーマットを設定
+	DXGI_FORMAT textureFormat = depthStencilFormat;
+	DXGI_FORMAT resourceFormat = depthStencilFormat;
+	//設定したフォーマットを入れ替え
+	switch (depthStencilFormat)
+	{
+	case DXGI_FORMAT_D16_UNORM:
+		textureFormat = DXGI_FORMAT_R16_TYPELESS;
+		resourceFormat = DXGI_FORMAT_R16_UNORM;
+		break;
+	case DXGI_FORMAT_D24_UNORM_S8_UINT:
+		textureFormat = DXGI_FORMAT_R24G8_TYPELESS;
+		resourceFormat = DXGI_FORMAT_R24_UNORM_X8_TYPELESS;
+		break;
+	case DXGI_FORMAT_D32_FLOAT:
+		textureFormat = DXGI_FORMAT_R32_TYPELESS;
+		resourceFormat = DXGI_FORMAT_R32_FLOAT;
+		break;
+	case DXGI_FORMAT_D32_FLOAT_S8X24_UINT:
+		textureFormat = DXGI_FORMAT_R32G8X24_TYPELESS;
+		resourceFormat = DXGI_FORMAT_R32_FLOAT_X8X24_TYPELESS;
+		break;
+	}
+	// 深度ステンシルを作成
+	ID3D11Texture2D* depthStencil = nullptr;
+	D3D11_TEXTURE2D_DESC depthStencilDesc = {};
+	depthStencilDesc.Width = swapChainDesc.BufferDesc.Width;
+	depthStencilDesc.Height = swapChainDesc.BufferDesc.Height;
+	depthStencilDesc.MipLevels = 1;
+	depthStencilDesc.ArraySize = 1;
+	depthStencilDesc.Format = textureFormat;
+	depthStencilDesc.SampleDesc = swapChainDesc.SampleDesc;
+	depthStencilDesc.Usage = D3D11_USAGE_DEFAULT;
+	depthStencilDesc.BindFlags =
+		D3D11_BIND_DEPTH_STENCIL |
+		D3D11_BIND_SHADER_RESOURCE;	// シェーダーリソースとして使用することを設定
+	depthStencilDesc.CPUAccessFlags = 0;
+	depthStencilDesc.MiscFlags = 0;
+	hr = graphicsDevice->CreateTexture2D(&depthStencilDesc, NULL, &depthStencil);
+	//失敗したら処理しない
+	if (FAILED(hr)) {
+		return 0;
+	}
+	// 深度ステンシルにアクセスするためのビューを作成
+	D3D11_DEPTH_STENCIL_VIEW_DESC depthStencilViewDesc = {};
+	depthStencilViewDesc.Format = depthStencilFormat;
+	if (depthStencilDesc.SampleDesc.Count > 0) {
+		depthStencilViewDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2DMS;
+	}
+	else {
+		depthStencilViewDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+		depthStencilViewDesc.Texture2D.MipSlice = 0;
+	}
+	depthStencilViewDesc.Texture2D.MipSlice = 0;
+	hr = graphicsDevice->CreateDepthStencilView(depthStencil, &depthStencilViewDesc, &depthStencilView);
+	//失敗したら処理しない
+	if (FAILED(hr)) {
+		return 0;
+	}
+	// 深度ステンシルにシェーダーからアクセスするためのリソース ビューを作成
+	D3D11_SHADER_RESOURCE_VIEW_DESC depthStencilResourceViewDesc = {};
+	depthStencilResourceViewDesc.Format = resourceFormat;
+	if (depthStencilDesc.SampleDesc.Count > 0) {
+		depthStencilResourceViewDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2DMS;
+	}
+	else {
+		depthStencilResourceViewDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+		depthStencilResourceViewDesc.Texture2D.MostDetailedMip = 0;
+		depthStencilResourceViewDesc.Texture2D.MipLevels = 1;
+	}
+	hr = graphicsDevice->CreateShaderResourceView(
+		depthStencil,
+		&depthStencilResourceViewDesc,
+		&depthStencilResourceView);
+	//失敗したら処理しない
+	if (FAILED(hr)) {
+		return 0;
+	}
+	SAFE_RELEASE(depthStencil);
+
+	// ビューポートを指定
+	viewports[0].Width = static_cast<FLOAT>(ScreenWidth);
+	viewports[0].Height = static_cast<FLOAT>(ScreenHeight);
+	viewports[0].MinDepth = 0.0f;
+	viewports[0].MaxDepth = 1.0f;
+	viewports[0].TopLeftX = 0.0f;
+	viewports[0].TopLeftY = 0.0f;
+
+	return true;
+}
+
+// 使用したグラフィックリソースを解放。
+void Game::ReleaseGraphicsDevice()
+{
+	SAFE_RELEASE(depthStencilResourceView);
+	SAFE_RELEASE(depthStencilView);
+	SAFE_RELEASE(renderTargetResourceView);
+	SAFE_RELEASE(renderTargetViews[0]);
+	SAFE_RELEASE(swapChain);
+	SAFE_RELEASE(immediateContext);
+	SAFE_RELEASE(graphicsDevice);
+}
+
+// メッセージループを実行
+int Game::Run()
+{
+	// ウィンドウを作成
+	if (!InitWindow()) {
+		MessageBox(NULL, L"ウィンドウの作成に失敗しました。", L"メッセージ", MB_OK);
+		return -1;
+	}
+	// グラフィックデバイスを作成
+	if (!InitGraphicsDevice()) {
+		MessageBox(NULL, L"グラフィックデバイスを初期化できませんでした。", L"メッセージ", MB_OK);
 		return -1;
 	}
 
 	// メッセージループを実行
 	MSG msg = {};
 	while (true) {
+		// レンダーターゲットを設定
+		immediateContext->OMSetRenderTargets(1, renderTargetViews, depthStencilView);
+		// 画面をクリアー
+		immediateContext->ClearRenderTargetView(renderTargetViews[0], clearColor);
+		immediateContext->ClearDepthStencilView(depthStencilView,
+			D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+		// ビューポートを設定
+		immediateContext->RSSetViewports(1, viewports);
+
+
+		// Direct3Dによる描画処理
+
+
+		// バックバッファーに描画したイメージをディスプレイに表示
+		HRESULT hr = S_OK;
+		hr = swapChain->Present(1, 0);
+		if (FAILED(hr))
+		{
+			MessageBox(hWnd,
+				L"グラフィックデバイスが物理的に取り外されたか、ドライバーがアップデートされました。",
+				L"エラー", MB_OK);
+			return -1;
+		}
+
 		// このウィンドウのメッセージが存在するかを確認
 		if (PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE)) {
 			// メッセージを取得
@@ -154,8 +302,7 @@ int Game::Run()
 		}
 	}
 
-	SAFE_RELEASE(immediateContext);
-	SAFE_RELEASE(graphicsDevice);
+	ReleaseGraphicsDevice();
 
 	return (int)msg.wParam;
 }
--- List0104/List0104/Game.h
+++ List0104/List0104/Game.h
@@ -5,14 +5,19 @@
 #pragma once
 
 #include <Windows.h>
+#include <d3d11.h>
 
 #define SAFE_RELEASE(p) if ((p) != nullptr) { (p)->Release(); (p) = nullptr; }
 
 // アプリケーション全体を表します。
 class Game {
-	//private:	
-	// クラスでアクセス指定子を省略するとprivateになります。
-	
+public:
+	// 必要な情報を上書きすることでアプリケーションを初期化。
+	void Initialize(LPCWSTR windowTitle, int screenWidth, int screenHeight);
+	// メッセージループを実行。
+	int Run();
+
+private:
 	// ウィンドウのタイトル
 	LPCWSTR WindowTitle = L"タイトル";
 	// ウィンドウの幅
@@ -22,12 +27,34 @@ class Game {
 	// ウィンドウのハンドル
 	HWND hWnd = NULL;
 
-public:
-	// 必要な情報を上書きすることでアプリケーションを初期化します。
-	void Initialize(LPCWSTR windowTitle, int screenWidth, int screenHeight);
-	// メッセージループを実行します。
-	int Run();
-
-private:
+	//ウィンドウの作成
 	bool InitWindow();
+
+	// Direct3D 11のデバイス(パソコンのグラフィック機能そのもの、GPU)
+	ID3D11Device* graphicsDevice = nullptr;
+	// Direct3D 11のデバイス コンテキスト
+	ID3D11DeviceContext* immediateContext = nullptr;
+	// Direct3D 11の機能レベル
+	D3D_FEATURE_LEVEL featureLevel = {};
+	// スワップチェーン
+	IDXGISwapChain* swapChain = nullptr;
+	// レンダーターゲット
+	ID3D11RenderTargetView* renderTargetViews[1];
+	// バックバッファーをシェーダーで利用するためのリソース ビュー
+	ID3D11ShaderResourceView* renderTargetResourceView = nullptr;
+	// 深度ステンシルのフォーマット
+	const DXGI_FORMAT depthStencilFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	// 深度ステンシル
+	ID3D11DepthStencilView* depthStencilView = nullptr;
+	// 深度ステンシルをシェーダーで利用するためのリソース ビュー
+	ID3D11ShaderResourceView* depthStencilResourceView = nullptr;
+	// 画面クリアーに使用するカラー
+	FLOAT clearColor[4] = { 53 / 255.0f, 70 / 255.0f, 166 / 255.0f, 1.0f };
+	// ビューポート
+	D3D11_VIEWPORT viewports[1] = {};
+
+	// グラフィックデバイスを作成。Game.cpp参照
+	bool InitGraphicsDevice();
+	// グラフィックリソースを解放します。Game.cpp参照
+	void ReleaseGraphicsDevice();
 };
